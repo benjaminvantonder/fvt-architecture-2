efdac2678b307e24753ec81199d041176804dfb0
benjaminvantonder <vissievanafrika@gmail.com>
2025-04-07 21:38:46 +0200

    first completed website

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let Projects_Btn = document.getElementById("projects-btn");
 let Contact_Btn = document.getElementById("contact-btn");
 let Contact_Btn_Bottom = document.getElementById("contact-btn-bottom");
 let Contact_Btn_Services = document.getElementById("contact-btn-services");
+let Contact_Btn_Projects = document.getElementById("contact-btn-projects");
+let Services_btn_About = document.getElementById("services-btn-on-about");
 
 // now let's link the ids of the pages
 
@@ -148,4 +150,34 @@ Contact_Btn_Services.addEventListener("click", function () {
   Services_Btn.classList.remove("red-letters");
   Projects_Btn.classList.remove("red-letters");
   Contact_Btn.classList.add("red-letters");
+});
+
+Contact_Btn_Projects.addEventListener("click", function () {
+  Home_Page.classList.add("hide");
+  About_Page.classList.add("hide");
+  Services_Page.classList.add("hide");
+  Projects_Page.classList.add("hide");
+  Contact_Page.classList.remove("hide");
+
+  Home_Btn.classList.remove("red-letters");
+  About_Btn.classList.remove("red-letters");
+  Services_Btn.classList.remove("red-letters");
+  Projects_Btn.classList.remove("red-letters");
+  Contact_Btn.classList.add("red-letters");
+});
+
+Services_btn_About.addEventListener("click", function () {
+  Home_Page.classList.add("hide");
+  About_Page.classList.add("hide");
+  Services_Page.classList.remove("hide");
+  Projects_Page.classList.add("hide");
+  Contact_Page.classList.add("hide");
+
+  NavbarDropdown.classList.remove("show");
+
+  Home_Btn.classList.remove("red-letters");
+  About_Btn.classList.remove("red-letters");
+  Services_Btn.classList.add("red-letters");
+  Projects_Btn.classList.remove("red-letters");
+  Contact_Btn.classList.remove("red-letters");
 });
